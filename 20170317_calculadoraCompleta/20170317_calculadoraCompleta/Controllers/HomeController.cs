1e282c20363c7260cbe193b98fc8352ec809aa1b
aulas <casimiro@ipt.pt>
2017-03-17 19:46:35 +0000

    definiçao e construção do 'layout' da calculadora

---
+++ 20170317_calculadoraCompleta/20170317_calculadoraCompleta/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _20170317_calculadoraCompleta.Controllers {
+    public class HomeController : Controller {
+        // GET: Home
+        public ActionResult Index() {
+            return View();
+        }
+
+
+        // POST: Home
+        /// <summary>
+        /// recebe os dados da View e processa-os
+        /// com vista a calcular o resultado a apresentar
+        /// ao utilizador
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Index(
+            string bt) {
+
+
+
+
+
+            return View();
+        }
+    }
+}
